b2003e9adc80f06ff11020c575b1f03199a71e39
Randy Morris <randy.morris@rsontech.net>
2013-01-22 18:40:44 +0000

    Source bash_completion on Ubuntu to get __git_ps1

--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,10 @@
-export PS1='\w $'
+export PS1='\w $ '
 
-if [ -f /usr/share/git/git-prompt.sh ]; then
-    source /usr/share/git/git-prompt.sh
+source /etc/bash_completion 2> /dev/null
+source /usr/share/git/git-prompt.sh 2> /dev/null
+
+if $(type -t __git_ps1 > /dev/null); then
     export GIT_PS1_SHOWDIRTYSTATE=yes
     export PS1='\w$(__git_ps1 " [%s]") $ '
 fi
+
