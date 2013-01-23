37a9a5aad0103369b2c33575e8ecb08c44c03300
Randy Morris <randy.morris@rsontech.net>
2013-01-23 20:34:34 +0000

    Use /bin/cat as pager inside emacs

--- environment.sh
+++ environment.sh
@@ -4,8 +4,6 @@ export HISTCONTROL="ignoreboth:erasedups"
 export HISTSIZE=1000
 export HISTFILESIZE=2000
 
-export EDITOR="emacsclient -c -a ''"
-
 if [[ -x /usr/bin/src-hilite-lesspipe.sh ]]; then
     export LESSOPEN="|/usr/bin/src-hilite-lesspipe.sh %s"
 fi
@@ -13,3 +11,8 @@ export LESS="-RFX"
 export LESSHISTFILE=~/.history/less
 
 export PATH=~/bin/:$PATH
+
+if [[ "$INSIDE_EMACS" =~ "comint" ]]; then
+    export GIT_PAGER=/bin/cat
+fi
+export EDITOR="emacsclient -c -a ''"
