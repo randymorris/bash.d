7bea4256f64432fcb54995ca78c6a7de617daa19
Randy Morris <randy.morris@rsontech.net>
2014-06-13 12:12:42 +0000

    in a working tree takes you to the working root.
    Repeat  again to go to ~.

--- functions.sh
+++ functions.sh
@@ -10,3 +10,17 @@ sp () {
 shop () {
     namei -lmv "$(readlink -f "$1")"
 }
+
+# `cd` with no arguments will first attempt to change to the root of
+#  the current working copy.  If already at the root or no working
+#  copy is found, cd to $HOME.
+function cd() {
+    root=$(wcroot)
+    if [ $# -ne 0 ]; then
+        builtin cd "$1"
+    elif [ -n "$root" -a "$PWD" != "$root" ]; then
+        builtin cd "$root"
+    else
+        builtin cd
+    fi
+}
