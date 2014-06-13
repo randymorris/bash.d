5589f3132b9a61212f2026c892c8a1cf1c394362
Randy Morris <randy.morris@rsontech.net>
2014-06-13 12:13:31 +0000

    Git branch should be first in the prompt
    This makes branch names with  in them less confusing

--- prompt.sh
+++ prompt.sh
@@ -5,6 +5,6 @@ source /usr/share/git/git-prompt.sh 2> /dev/null
 
 if $(type -t __git_ps1 > /dev/null); then
     export GIT_PS1_SHOWDIRTYSTATE=yes
-    export PS1='\w$(__git_ps1 " [%s]") $ '
+    export PS1='$(__git_ps1 "[%s]") \w $ '
 fi
 
