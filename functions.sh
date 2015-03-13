63708c5f6f85a4419eda1498a93c5c37a30e2d03
Randy Morris <randy.morris@rsontech.net>
2015-03-13 18:31:56 -0400

    Handle symlinks more cleanly

--- functions.sh
+++ functions.sh
@@ -9,14 +9,22 @@ shop () {
 function cd() {
     if which wcroot &> /dev/null; then
         root=$(wcroot)
+        canonical_wd=$(readlink -f "$PWD")
         if [ $# -ne 0 ]; then
             builtin cd "$1"
-        elif [ -n "$root" -a "$PWD" != "$root" ]; then
-            builtin cd "$root"
+        elif [ -n "$root" -a "$canonical_wd" != "$root" ]; then
+            # `wcroot` always returns the canonical path to the root
+            # of the working copy.  We don't want to cd to the
+            # canonical path if the current directory is a symlink.
+            # Instead, stay with the symlink by extracting it from
+            # $PWD.
+            subdir="${canonical_wd#$root}"
+            basedir="${PWD%$subdir}"
+            builtin cd "$basedir"
         else
             builtin cd
         fi
     else
-        builtin cd $*
+        builtin cd "$*"
     fi
 }
