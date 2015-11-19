da577d728ed44d196c7bd3fd9cc7e1d4f81873d5
Randy Morris <randy.morris@rsontech.net>
2015-11-19 08:59:12 -0500

    Update paths for bash completion/git prompt

--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,7 @@
 export PS1='\w $ '
 
 source /etc/bash_completion 2> /dev/null
-source /usr/share/git/git-prompt.sh 2> /dev/null
+source /usr/local/etc/bash_completion 2> /dev/null
 
 if $(type -t __git_ps1 > /dev/null); then
     export GIT_PS1_SHOWDIRTYSTATE=yes
